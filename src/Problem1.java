e0297d6f4dafee280063918b1c66cbecad170751
KailasChandran <kailaskochery@gmail.com>
2026-04-16 11:17:38 +0530

    Added Level 2 programs: Odd-Even and Employee Bonus

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
-class DivisibleByFive {
+class OddEvenNumbers {
     public static void main(String[] args) {
 
         try (Scanner input = new Scanner(System.in)) {
-
             System.out.print("Enter a number: ");
             int number = input.nextInt();
 
-            boolean isDivisible = (number % 5 == 0);
-
-            System.out.println("Is the number " + number + " divisible by 5? " + isDivisible);
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
         }
     }
 }
